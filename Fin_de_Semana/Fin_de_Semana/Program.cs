4a1afac9d7298b6a8d87ef6f736e7027469de2ad
jannabel <jannabelramos@hotmail.com>
2020-06-02 19:27:12 -0400

    Parcial

---
+++ Fin_de_Semana/Fin_de_Semana/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fin_de_Semana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un programa que pida el nombre de un dia de la semana e identifique 
+                 si es fin de semana o no. En caso de error, indicarlo*/
+
+            Console.Write("\n ~ Ingrese un dia la semana: ");
+            string dia = Console.ReadLine();
+
+            if (dia == "Sabado" || dia == "Domingo")
+            {
+                Console.WriteLine(" \n ~ ES FIN DE SEMANA ~ ");
+            }
+            else if(dia == "Lunes" || dia == "Martes" || dia == "Miercoles" || dia == "Jueves" || dia == "Viernes")
+            {
+                Console.WriteLine(" \n ~ NO es fin de semana ~ ");
+            }
+            else
+            {
+                Console.WriteLine(" \n ~ Error ~ ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Parcial 1/Parcial 1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Parcial_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Pedir al usuario el precio de un producto y la forma de pagar(efectivo o tarjeta)
+             si la forma de pago es mediante tarjeta, pedir el numero de tarjeta(12 digitos). 
+             Ojo, validar que el valor ingresado en el sea positivo.*/
+
+
+
+             Console.Write(" \n ~ Ingrese el precio del producto: ");
+             int precio = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n  ~Forma de pago: ");
+            Console.WriteLine("  1- Tarjeta ");
+            Console.WriteLine("  2- Efectivo ");
+            Console.Write(" ~ ");
+            int formadepago = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (formadepago)
+            {
+                case 1:
+                    Console.Write("\n  ~Ingrese el numero de su tarjeta: ");
+                    long tarjeta = Convert.ToInt64(Console.ReadLine());
+                    if (tarjeta < 0)
+                    {
+                        Console.WriteLine("  ~El numero es incorrecto ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n  ~ Su pago se ha hecho satisfactoriamente ~ ");
+                        Console.WriteLine("  ~ Gracias por su compra, vuelva pronto ~ ");
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("\n  ~ Su pago se ha hecho satisfactoriamente ~ ");
+                    Console.WriteLine("\n  ~ Gracias por su compra, vuelva pronto ~ ");
+                    break;
+            }
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
